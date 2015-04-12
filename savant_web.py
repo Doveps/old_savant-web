9152087e97cf3f9174348461b44c11c3b260a294
Kurt Yoder <kyoder@doveps.com>
2015-04-12 13:42:21 -0400

    Attach to savant
    Use the diffs ZODB.

--- savant_web.py
+++ savant_web.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+import argparse
+
 from flask import Flask, g, redirect, url_for, render_template, flash, request
+
+from savant.inferences import db
 
 # configuration
 DEBUG = True
@@ -10,27 +14,30 @@ app.config.from_object(__name__)
 
 @app.before_request
 def before_request():
-    g.db = None
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        pass
+    g.db = db.DB(args.inference_db)
 
 @app.route('/')
 def home():
-    return render_template('home.html', diffs=[])
+    return render_template('home.html', diffs=g.db.get_diffs())
 
-@app.route('/diff/<uuid>')
-def diff(uuid=None):
-    return render_template('diff.html', uuid=uuid)
+@app.route('/diff/<id>')
+def diff(id=None):
+    return render_template('diff.html', id=id)
 
 @app.route('/add', methods=['POST'])
 def add():
-    flash('New diff was successfully added')
-    return redirect(url_for('diff', uuid=request.form['uuid']))
+    flash('New set added using diff')
+    return redirect(url_for('diff', id=request.form['id']))
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+            description='App to assign diffs to sets')
+    arg_parser.add_argument(
+            '-i', '--inference-db',
+            required=True,
+            help='The path to the directory containing the inference ZODB \
+                    files')
+    args = arg_parser.parse_args()
+
     app.run()
 

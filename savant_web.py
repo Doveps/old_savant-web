b01bebff829650d122f2497a86ea0e5a7e1bd34e
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-12-01 06:10:41 +0800

    Initialize app

--- savant_web.py
+++ savant_web.py
@@ -29,8 +29,8 @@ app.config['DEBUG_TB_PANELS'] = [
     'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
     'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
     'flask_debugtoolbar.panels.template.TemplateDebugPanel',
-    #'flask_debugtoolbar.panels.sqlalchemy.SQLAlchemyDebugPanel',
-    #'flask_debugtoolbar.panels.logger.LoggingPanel',
+    # 'flask_debugtoolbar.panels.sqlalchemy.SQLAlchemyDebugPanel',
+    # 'flask_debugtoolbar.panels.logger.LoggingPanel',
     'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel',
     'flask_debugtoolbar_lineprofilerpanel.panels.LineProfilerPanel'
 ]
@@ -42,31 +42,36 @@ try:
 except ConfigParser.NoSectionError:
     # probably no log.conf file
     logging.basicConfig(
-            format='%(message)s',
-            )
+        format='%(message)s',
+    )
 
 logger = logging.getLogger(__name__)
 
 try:
     from log_override import LOG_OVERRIDES
+
     logging.config.dictConfig(LOG_OVERRIDES)
 except:
     logger.debug('unable to load log_override; ignoring')
 
+
 @app.before_request
 def before_request():
     g.db = savant.db.DB(args.inference_db)
+
 
 @app.after_request
 def after_request(response):
     g.db.close()
     return response
 
+
 @app.route('/')
 def home():
     return render_template(
-            'home.html',
-            )
+        'home.html',
+    )
+
 
 @app.route('/comparisons')
 def comparisons():
@@ -82,11 +87,12 @@ def comparisons():
             incomplete.append(comp_obj.id)
 
     return render_template(
-            'comparisons.html',
-            comparisons = all_comps,
-            complete = complete,
-            incomplete = incomplete,
-            )
+        'comparisons.html',
+        comparisons=all_comps,
+        complete=complete,
+        incomplete=incomplete,
+    )
+
 
 @app.route('/comparison/<id>')
 def comparison(id=None):
@@ -107,11 +113,12 @@ def comparison(id=None):
     dform = forms.DDiffNamingForm(g.db, id, exclude_set_ids=exclude_set_ids)
 
     return render_template(
-            'comparison.html',
-            id = id,
-            sets = sets,
-            form = dform.form,
-            )
+        'comparison.html',
+        id=id,
+        sets=sets,
+        form=dform.form,
+    )
+
 
 @app.route('/sets')
 def sets():
@@ -135,12 +142,13 @@ def sets():
         other_sets = [savant.sets.Set(g.db, s) for s in other_sets]
 
         data = {
-                'set': set_obj,
-                'comparisons': comparisons,
-                'other_sets': other_sets,
-                }
+            'set': set_obj,
+            'comparisons': comparisons,
+            'other_sets': other_sets,
+        }
         sets.append(data)
     return render_template('sets.html', sets=sets)
+
 
 @app.route('/set/edit/<escaped_id>')
 def set_edit(escaped_id=None):
@@ -156,14 +164,15 @@ def set_edit(escaped_id=None):
     comparisons = [savant.comparisons.Comparison(g.db, c) for c in comparisons]
 
     return render_template(
-            'set.html',
-            set = set_obj,
-            comparisons = comparisons,
-            form = dform.form)
+        'set.html',
+        set=set_obj,
+        comparisons=comparisons,
+        form=dform.form)
+
 
 @app.route('/set/add', methods=['POST'])
 def set_add():
-    id=request.form['id']
+    id = request.form['id']
     dform = forms.DDiffNamingForm(g.db, id, request)
     if len(dform.set_choices) == 0:
         flash(Markup('Did you forget to choose diffs?'))
@@ -171,57 +180,60 @@ def set_add():
 
     new_set = savant.sets.Set(g.db, dform.set_id)
     new_set.update_diffs(dform.set_choices)
-    message = Markup('New set; action: <strong>%s</strong>; system: <strong>%s</strong>; name: <strong>%s</strong>; diffs: <strong>%s</strong>' %
+    message = Markup(
+        'New set; action: <strong>%s</strong>; system: <strong>%s</strong>; name: <strong>%s</strong>; diffs: <strong>%s</strong>' %
         (
-        dform.form.action.data,
-        dform.form.system.data,
-        dform.form.name.data,
-        str(len(dform.set_choices)),
+            dform.form.action.data,
+            dform.form.system.data,
+            dform.form.name.data,
+            str(len(dform.set_choices)),
         ))
     flash(message)
     return redirect(url_for('comparison', id=id))
 
+
 @app.route('/set/update_diffs', methods=['POST'])
 def set_update_diffs():
-    escaped_id=request.form['escaped_id']
+    escaped_id = request.form['escaped_id']
     set_id = urllib.unquote(escaped_id)
     dform = forms.DDiffForm(g.db, set_id, request)
     for diff in dform.delete_diffs:
         diff_obj = savant.diffs.Diff(diff)
         dform.set_obj.delete_diff(diff_obj)
     message = Markup('Set updated: <strong>%s %s: %s</strong>' %
-            (
-                dform.set_obj.info.action,
-                dform.set_obj.info.system,
-                dform.set_obj.info.name
-                ))
+                     (
+                         dform.set_obj.info.action,
+                         dform.set_obj.info.system,
+                         dform.set_obj.info.name
+                     ))
     flash(message)
     return redirect(url_for('set_edit', escaped_id=escaped_id))
 
+
 @app.route('/set/delete', methods=['POST'])
 def set_delete():
-    escaped_id=request.form['escaped_id']
+    escaped_id = request.form['escaped_id']
     set_id = urllib.unquote(escaped_id)
     set_obj = savant.sets.Set(g.db, set_id)
     set_obj.delete()
     message = Markup('Set deleted: <strong>%s %s: %s</strong>' %
-            (
-                set_obj.info.action,
-                set_obj.info.system,
-                set_obj.info.name
-                ))
+                     (
+                         set_obj.info.action,
+                         set_obj.info.system,
+                         set_obj.info.name
+                     ))
     flash(message)
     return redirect(url_for('sets'))
 
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
-            description='App to assign diffs to sets')
+        description='App to assign diffs to sets')
     arg_parser.add_argument(
-            '-i', '--inference-db',
-            required=True,
-            help='The path to the directory containing the inference ZODB \
+        '-i', '--inference-db',
+        required=True,
+        help='The path to the directory containing the inference ZODB \
                     files')
     args = arg_parser.parse_args()
 
     app.run()
-

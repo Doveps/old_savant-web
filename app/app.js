18e0ce305b7df0424de47fccfb3ea9763ee58801
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-12-01 06:28:57 +0800

    Update app

--- app/app.js
+++ app/app.js
@@ -3,39 +3,21 @@ angular
         /*
          * Angular modules
          */
-        'ngResource', 'ui.router', 'ngStorage', 'ngSanitize',
+        'ngResource', 'ui.router'
 
         /*
          * 3rd Party modules
          */
-        'angular-loading-bar',
-        'ngAnimate',
-        'oc.lazyLoad',
-        'ui.date',
-        'LocalStorageModule',
-        'ui.bootstrap',
-        'infinite-scroll',
-
-        'datatables',
-        'datatables.fixedcolumns',
-        'datatables.select',
-        'datatables.columnfilter',
-        // 'datatables.light-columnfilter',
-        'angular.filter'
-
     ])
     .config(routeConfig)
     .run(run);
 
-function routeConfig($urlRouterProvider, $httpProvider, $resourceProvider, cfpLoadingBarProvider) {
+function routeConfig($urlRouterProvider, $httpProvider, $resourceProvider) {
     // Enable cors in client side
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-    // Configure loading bar
-    cfpLoadingBarProvider.includeBar = false;
-
-    $urlRouterProvider.otherwise("/login");
+    $urlRouterProvider.otherwise("/");
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }
 
